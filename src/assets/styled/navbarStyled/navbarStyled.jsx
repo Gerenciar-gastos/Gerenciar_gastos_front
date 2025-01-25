701ad3645455e63e7d523621cca59586812eeb09
Monique de Souza <moniquejp16@gmail.com>
2025-01-25 19:07:40 -0300

    feat: close logged out box created

--- src/assets/styled/navbarStyled/navbarStyled.jsx
+++ src/assets/styled/navbarStyled/navbarStyled.jsx
@@ -22,6 +22,7 @@ export const Conteiner1 = styled.div`
   align-items: center;
   border-radius: 50%;
   margin-left: 10%;
+  cursor: pointer;
   img{
     width: 60px;  
     height: 50px; 
@@ -38,8 +39,49 @@ export const Profile = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
   svg {
     color: #e30de3; 
     font-size: 2rem; 
   }
 `;
+
+export const LogOut = styled.div`
+  background-color: rgba(255, 255, 255, 0.5);
+  width: 300px;  
+  height: 300px;
+  position : fixed ;
+  top: 70px;
+  right: 60px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  svg {
+    color: #e30de3; 
+    font-size: 2rem;
+    margin-right :10px ;
+    margin-top: 10px;
+  }
+`;
+export const Close = styled.div`
+  width:300px ;  
+  height: 50px;
+  display: flex;
+  justify-content: flex-end;
+  cursor: pointer;
+`;
+
+export const HandleLogout = styled.button`
+  margin-top:  10px;
+  width: 200px;  
+  height: 100px;
+  cursor: pointer;
+  color: #e30de3;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 50px;
+  margin-bottom: 10px;
+`
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,45 @@
 
-import { All, Conteiner1, Profile } from "../assets/styled/navbarStyled/navbarStyled";
+import { All, Conteiner1, Profile, LogOut, Close, HandleLogout } from "../assets/styled/navbarStyled/navbarStyled";
 import craiyon from "../assets/images/craiyon.png"
 import { useNavigate } from "react-router-dom";
 import { FaUser } from "react-icons/fa6";
+import { useState } from "react";
+import { IoCloseCircleSharp } from "react-icons/io5";
 
 
 export default function NavBar() {
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
+    const [openProfile, setOpenProfile] = useState(false)
+
+    function profile() {
+        setOpenProfile(true)
+    }
+    function profileLogOut() {
+        setOpenProfile(false)
+    }
+    function handleLogout() {
+        localStorage.removeItem("authToken");
+        navigate("/");
+    }
 
     return (
         <All>
-            <Conteiner1 onClick={navigate('/home')}>
-                <img src={craiyon} onClick={navigate('/home') }/>
+            <Conteiner1 onClick={() => navigate('/home')}>
+                <img src={craiyon} />
             </Conteiner1>
-            <Profile>
+            <Profile onClick={() => profile()}>
                 <FaUser />
             </Profile>
+            {openProfile && (
+                <LogOut >
+                    <Close>
+                        <IoCloseCircleSharp onClick={() => profileLogOut()} />
+                    </Close>
+                    <HandleLogout onClick={() => handleLogout()} >
+                        Logout
+                    </HandleLogout>
+                </LogOut>
+            )}
         </All >
     );
 }
