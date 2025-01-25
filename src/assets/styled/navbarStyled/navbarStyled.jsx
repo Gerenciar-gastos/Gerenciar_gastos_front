8087c79e4f56836441c8650231b7830455bcf8ec
Monique de Souza <moniquejp16@gmail.com>
2025-01-25 17:51:49 -0300

    feat: profile e logo creat

--- src/assets/styled/navbarStyled/navbarStyled.jsx
+++ src/assets/styled/navbarStyled/navbarStyled.jsx
@@ -2,24 +2,44 @@ import styled from "styled-components";
 
 
 export const All = styled.div`
-  background-color: #f64809;
+  background-color: rgba(255, 255, 255, 0.5);
   color: #a70ee9;
   width: 100vw;  
-  height: 50px; 
+  height: 60px; 
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   position: fixed;
   top: 0;
 `;
 
-export const Conteiner = styled.div`
+export const Conteiner1 = styled.div`
   background-color: rgba(255, 255, 255, 0.5);
   width: 60px;  
   height: 50px; 
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 10px;
+  border-radius: 50%;
+  margin-left: 10%;
+  img{
+    width: 60px;  
+    height: 50px; 
+    border-radius: 50%;
+  }
 `;
 
+export const Profile = styled.div`
+  background-color: rgba(255, 255, 255, 0.5);
+  width: 60px;  
+  height: 50px; 
+  margin-right: 10%;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    color: #e30de3; 
+    font-size: 2rem; 
+  }
+`;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,21 @@
 
-import { All, Conteiner } from "../assets/styled/navbarStyled/navbarStyled";
+import { All, Conteiner1, Profile } from "../assets/styled/navbarStyled/navbarStyled";
 import craiyon from "../assets/images/craiyon.png"
+import { useNavigate } from "react-router-dom";
+import { FaUser } from "react-icons/fa6";
 
 
 export default function NavBar() {
+    const navigate = useNavigate(); 
 
     return (
         <All>
-            <Conteiner>
-                <image src={craiyon}/>
-            </Conteiner>
+            <Conteiner1 onClick={navigate('/home')}>
+                <img src={craiyon} onClick={navigate('/home') }/>
+            </Conteiner1>
+            <Profile>
+                <FaUser />
+            </Profile>
         </All >
     );
 }
