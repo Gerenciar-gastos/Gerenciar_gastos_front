d379b176acf6c8b7dded553ee5e790bc54c90001
Monique de Souza <moniquejp16@gmail.com>
2025-01-30 19:10:59 -0300

    feat: taking bank information and distributing it on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AuthProvider from "./contexts/UserContex.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Home from "./pages/Home.jsx";
+import AuthProvider from "./contexts/contex.jsx";
 
 
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,43 @@
+import { useContext, useEffect, useState } from "react";
 import { All, Month, Name, Percentage } from "../assets/styled/homeStyled/monthStyled";
 import NavBar from "../components/Navbar";
+import { AuthContext } from "../contexts/contex";
+import axios from "axios";
+
 
 export default function Home() {
+ 
+    const { token } = useContext(AuthContext);
+    const [authToken, setAuthToken] = useState(localStorage.getItem("authToken") || token);
+    const [data, setData] = useState(null);
+
+
+    useEffect(() => {
+        const urlCode = `${import.meta.env.VITE_API_URL}/home`;
+
+        axios.get(urlCode, {
+            headers: { Authorization: `Bearer ${authToken}` } 
+        })
+            .then((response) => {
+                console.log("Dados recebidos:", response.data);
+                setData(response.data);
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar dados:", error);
+            });
+    }, [authToken]);
 
     return (
         <All>
             <NavBar />
-            <Month>
-                <Name>
-                    mes
-                </Name>
-                <Percentage>
-                    %
-                </Percentage>
-            </Month>
-            <Month>
-                <Name>
-                    mes
-                </Name>
-                <Percentage>
-                    %
-                </Percentage>
-            </Month>
-            <></>
-
+            {data?.Month?.map((month) => (
+                <Month key={month.id}>
+                    <Name>{month.name}</Name>
+                    <Percentage>
+                        {((month.totalSpent / month.totalFunds) * 100).toFixed(2)}%
+                    </Percentage>
+                </Month>
+            ))}
         </All>
 
     );
