81b0740a0c2ea467a3e3ea0f17ece210b84e86a7
Monique de Souza <moniquejp16@gmail.com>
2025-02-19 12:02:52 -0300

    feat: implementing the containers

--- src/assets/styled/monthEditionStyled/monthEditionStyled.jsx
+++ src/assets/styled/monthEditionStyled/monthEditionStyled.jsx
@@ -6,6 +6,54 @@ export const All = styled.div`
  width: 100vw;  
  height: 100vh; 
  display: flex;
- justify-content: space-around;
+ justify-content: center;
  align-items: center;
 `;
+
+export const Conteiner = styled.div`
+ background-color: #f02816;
+ color: aliceblue;
+ width: 60vw;  
+ height: 80vh; 
+ margin-right: 10px;
+ margin-left: 10px;
+`;
+
+export const NameCard = styled.div`
+ background-color: #f0e516;
+ color: aliceblue;
+ width: 100%;  
+ height: 10%; 
+`;
+
+export const NamePersonValue = styled.div`
+ background-color: #5ff016;
+ color: aliceblue;
+ width: 100%;  
+ height: 10%; 
+ margin-top: 10px;
+ display: flex;
+`;
+
+export const Name = styled.div`
+ background-color: #16f0cc;
+ color: aliceblue;
+ width: 100%;  
+ height: 100%; 
+`;
+
+export const Person = styled.div`
+ background-color: #1654f0;
+ color: aliceblue;
+ width: 100%;  
+ height: 100%;
+`;
+
+export const Value = styled.div`
+ background-color: #e916f0;
+ color: aliceblue;
+ width: 100%;  
+ height: 100%;
+`;
+
+
--- src/pages/MonthEdition.jsx
+++ src/pages/MonthEdition.jsx
@@ -1,4 +1,4 @@
-import { All } from "../assets/styled/monthEditionStyled/monthEditionStyled";
+import { All, Conteiner, NameCard, NamePersonValue, Name, Person, Value } from "../assets/styled/monthEditionStyled/monthEditionStyled";
 
 
 export default function MonthEdition() {
@@ -6,7 +6,24 @@ export default function MonthEdition() {
 
     return (
       <All>
-        
+            <Conteiner>
+                <NameCard></NameCard>
+                <NamePersonValue>
+                    <Name></Name>
+                    <Person></Person>
+                    <Value></Value>
+                </NamePersonValue>
+            </Conteiner>
+            <Conteiner>
+                <NameCard></NameCard>
+                <NamePersonValue>
+                    <Name></Name>
+                    <Person></Person>
+                    <Value></Value>
+                </NamePersonValue>
+            </Conteiner>
+            
+            
       </All>  
     );
 }
