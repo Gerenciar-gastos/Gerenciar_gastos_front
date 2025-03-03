e8ff985ab3e2f64492a526266b097d9d1fd7a069
Monique de Souza <moniquejp16@gmail.com>
2025-03-03 16:28:10 -0300

    refactor:componentizing addEntry

--- src/components/MonthEdition/addEntry.jsx
+++ src/components/MonthEdition/addEntry.jsx
@@ -1,4 +1,5 @@
 export function addEntry(containerId, setContainers, containers) {
+    
     setContainers(containers.map(container =>
         container.id === containerId
             ? { ...container, entries: [...container.entries, { id: container.entries.length + 1, name: "", person: "", value: "" }] }
